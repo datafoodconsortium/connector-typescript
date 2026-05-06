4bd2b2c1c80457a928542ca08822dcd28d947d15
Maxime Lecoq-Gaillard <maxime@lecoqlibre.fr>
2026-05-06 11:23:39 +0200

    Release v1.0.0-alpha.11
    Fix properties of AsPlannedTransformation, AsPlannedConsumptionFlow and AsPlannedProductionFlow (#17).

--- lib/PlannedConsumptionFlow.d.ts
+++ lib/PlannedConsumptionFlow.d.ts
@@ -1,8 +1,8 @@
-import IQuantity from "./IQuantity.js";
+import Flow from "./Flow.js";
 import IDefinedProduct from "./IDefinedProduct.js";
 import IPlannedTransformation from "./IPlannedTransformation.js";
+import IQuantity from "./IQuantity.js";
 import IPlannedFlow from "./IPlannedFlow.js";
-import Flow from "./Flow.js";
 import IPlannedConsumptionFlow from "./IPlannedConsumptionFlow.js";
 import { Semanticable } from "@virtual-assembly/semantizer";
 import IConnector from "./IConnector.js";
@@ -17,9 +17,9 @@ export default class PlannedConsumptionFlow extends Flow implements IPlannedCons
         product?: IDefinedProduct;
         doNotStore?: boolean;
     });
+    getConsumedProduct(options?: IGetterOptions): Promise<IDefinedProduct | undefined>;
+    setConsumedProduct(consumedProduct: IDefinedProduct): void;
     getPlannedTransformation(options?: IGetterOptions): Promise<IPlannedTransformation | undefined>;
     setPlannedTransformation(plannedTransformation: IPlannedTransformation): void;
-    setConsumedProduct(consumedProduct: IDefinedProduct): void;
-    getConsumedProduct(options?: IGetterOptions): Promise<IDefinedProduct | undefined>;
 }
 //# sourceMappingURL=PlannedConsumptionFlow.d.ts.map
